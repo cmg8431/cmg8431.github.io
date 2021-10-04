2b2f706310bcb7d34ef89eccbc85a5cc58b7a5fa
cmg8431 <cmg8431@gmail.com>
2021-10-04 20:53:05 +0900

    Initial Commit

---
+++ src/components/Text.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import React, { FunctionComponent } from 'react';
+
+interface TextProps {
+  text: string;
+}
+
+const Text: FunctionComponent<TextProps> = ({ text }) => {
+  return <div>{text}</div>;
+};
+
+export default Text;
