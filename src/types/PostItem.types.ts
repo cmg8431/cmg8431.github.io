35ecae8729d396b91d4800ea429b621966a841a3
cmg8431 <cmg8431@gmail.com>
2021-10-27 14:55:17 +0900

    Post type separation

--- src/types/PostItem.types.ts
+++ src/types/PostItem.types.ts
@@ -1,16 +1,16 @@
 export type PostFrontmatterType = {
-    title: string
-    date: string
-    summary: string
+  title: string;
+  date: string;
+  summary: string;
+};
 
-  }
-  
-  export type PostListItemType = {
-    node: {
-      id: string
-      fields:{
-        slug: string
-      }
-      frontmatter: PostFrontmatterType
-    }
-  }
+export type PostListItemType = {
+  node: {
+    id: string;
+    fields: {
+      slug: string;
+    };
+    html: string;
+    frontmatter: PostFrontmatterType;
+  };
+};
