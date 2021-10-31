a10e2ac224bc24eeb52626ee28d733f33a5901e3
최민기 <cmg8431@gmail.com>
2021-10-31 21:56:09 +0900

    feat: Add a github comment

--- src/components/CommentWidget.tsx
+++ src/components/CommentWidget.tsx
@@ -1,11 +1,7 @@
 import React, { createRef, FunctionComponent, useEffect } from 'react';
 
 const src = 'http://mingi.tech/';
-<<<<<<< HEAD
 const repo = 'cmg8431/cmg8431.github.io';
-=======
-const repo = 'cmg8431/cmg8431.github.io'; // 자신 계정의 레포지토리로 설정
->>>>>>> 107c7822065757579e3c47fc70f4ea21813577d2
 
 type UtterancesAttributesType = {
   src: string;
@@ -17,22 +13,12 @@ type UtterancesAttributesType = {
   async: string;
 };
 
-<<<<<<< HEAD
 const CommentWidget: FunctionComponent = () => {
-=======
-const CommentWidget: FunctionComponent = function () {
->>>>>>> 107c7822065757579e3c47fc70f4ea21813577d2
   const element = createRef<HTMLDivElement>();
 
   useEffect(() => {
     if (element.current === null) return;
-<<<<<<< HEAD
     const utterances: HTMLScriptElement = document.createElement('script');
-=======
-
-    const utterances: HTMLScriptElement = document.createElement('script');
-
->>>>>>> 107c7822065757579e3c47fc70f4ea21813577d2
     const attributes: UtterancesAttributesType = {
       src,
       repo,
