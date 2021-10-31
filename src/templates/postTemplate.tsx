0b7af796e5776d1a09d1553c460c79484b25609b
최민기 <cmg8431@gmail.com>
2021-10-31 21:59:38 +0900

    feat: Change post template

--- src/templates/postTemplate.tsx
+++ src/templates/postTemplate.tsx
@@ -2,11 +2,6 @@ import React, { FunctionComponent } from 'react';
 import { graphql } from 'gatsby';
 import { PostListItemType } from 'types/PostItem.types';
 import PostContent from '../components/PostContent';
-import styled from 'styled-components';
-import HeaderComponent from 'components/Header';
-import { string } from 'prop-types';
-import LayoutComponent from 'components/Layout';
-import CommentWidget from 'components/CommentWidget';
 
 type PostTemplateProps = {
   data: {
@@ -29,16 +24,7 @@ const PostTemplate: FunctionComponent<PostTemplateProps> = ({
     },
   } = edges[0];
 
-<<<<<<< HEAD
   return <PostContent html={html} />;
-=======
-  return (
-    <>
-      <PostContent html={html} />
-      <CommentWidget />
-    </>
-  );
->>>>>>> 107c7822065757579e3c47fc70f4ea21813577d2
 };
 
 export default PostTemplate;
