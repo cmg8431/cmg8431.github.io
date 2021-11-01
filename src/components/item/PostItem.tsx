a2d80cb28ec853c53e15526b8d8f030e4c591a29
최민기 <cmg8431@gmail.com>
2021-11-02 00:07:08 +0900

    feat: Change post style and add hover click me

--- src/components/item/PostItem.tsx
+++ src/components/item/PostItem.tsx
@@ -1,39 +1,77 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
 import { PostFrontmatterType } from 'types/PostItem.types';
+import Thumbnail from '../../assets/img/PostImg.png';
 
 type PostItemProps = PostFrontmatterType & { link: string };
 
-const PostItem: FunctionComponent<PostItemProps> = function ({
+const PostItem: FunctionComponent<PostItemProps> = ({
   title,
   date,
   summary,
   link,
-}) {
+  thumbnail,
+}) => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
   return (
-    <PostItemWrapper to={link}>
-      <PostItemContent>
-        <Title>{title}</Title>
-        <Summary>{summary}</Summary>
-        <Date>{date}</Date>
-      </PostItemContent>
-    </PostItemWrapper>
+    <>
+      <PostItemWrapper to={link} onMouseEnter={onHover} onMouseLeave={onHover}>
+        <PostItemContent>
+          <ThumbnailImage src={Thumbnail} alt="post img" />
+          <Title>{title}</Title>
+          <Summary>{summary}</Summary>
+          <Date>{date}</Date>
+        </PostItemContent>
+        {hover ? <Text>click me</Text> : ''}
+      </PostItemWrapper>
+    </>
   );
 };
 
 const PostItemWrapper = styled(Link)`
+  z-index: 1;
   text-decoration-line: none;
   text-decoration: none;
   border-radius: 1rem;
   color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
+export const Text = styled.span`
+  text-align: center;
+  position: absolute;
+  margin-right: -6rem;
+  font-family: 'Times New Roman', Times, serif;
+  transform: translate(-50%, -50%);
+  backdrop-filter: blur(-4px);
+  filter: blur(-4px);
+  font-size: 1.5rem;
+  text-decoration: underline;
+`;
+
+const ThumbnailImage = styled.img`
+  width: 100%;
+  height: 300px;
+  border-radius: 10px;
+  margin-bottom: 1rem;
+  object-fit: cover;
+`;
 const PostItemContent = styled.div`
-  flex: 1;
   display: flex;
   flex-direction: column;
   padding: 1rem;
+  :hover {
+    backdrop-filter: blur(5px);
+    filter: blur(4px);
+    transition: 0.2s ease;
+    box-shadow: 1px 1px 100px #131313;
+  }
 `;
 
 const Title = styled.div`
@@ -56,10 +94,13 @@ const Summary = styled.div`
   text-overflow: ellipsis;
   white-space: normal;
   overflow-wrap: break-word;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  font-size: 16px;
+  opacity: 0.7;
   font-size: 1rem;
   line-height: 1.5;
   font-weight: 400;
-  opacity: 0.7;
   margin-bottom: 1rem;
 `;
 
