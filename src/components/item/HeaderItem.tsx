04b481b238490158b6d6a8c0d3f5cf5c4cbe229e
최민기 <cmg8431@gmail.com>
2021-11-02 00:05:46 +0900

    feat: git commit -m feat:

--- src/components/item/HeaderItem.tsx
+++ src/components/item/HeaderItem.tsx
@@ -1,15 +1,40 @@
-import React, { FC, FunctionComponent } from 'react';
+import React, { FC, FunctionComponent, useEffect } from 'react';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
+import { useState } from 'react';
+import { ClipModal } from 'components/ClipModal';
+import { useCopyClipboard } from 'hooks';
 
 const HeaderItemComponent: FunctionComponent = () => {
+  const [toastState, setToastState] = useState<boolean>(false);
+  const [clipboardState, copyToClipboard] = useCopyClipboard();
+
+  const copy = (): void => {
+    const { href } = location;
+    setToastState(true);
+    copyToClipboard(href);
+  };
+
+  const url = window.location.href;
+  // const ClipModalComponent = () => {
+  //   return (
+  //     <ClipModal
+  //       message={
+  //         clipboardState.isSuccess
+  //           ? '현재 페이지의 주소가 복사되었습니다!'
+  //           : '현재 페이지의 주소 복사를 실패했습니다.'
+  //       }
+  //       timeout={300}
+  //       state={toastState}
+  //       setState={setToastState}
+  //     />
+  //   );
+  // };
   return (
     <HeaderItemWapper>
-      <Link to={null}>
-        <HeaderLogo>MinGI.tech</HeaderLogo>
-      </Link>
+      <HeaderLogo to={url}>MinGI.tech</HeaderLogo>
       <HeaderSort />
-      <HeaderText>share</HeaderText>
+      <ShareButton onClick={copy}>share</ShareButton>
       <HeaderText>portfolio</HeaderText>
     </HeaderItemWapper>
   );
@@ -28,13 +53,22 @@ const HeaderText = styled.div`
   margin-left: 2rem;
 `;
 
-const HeaderSort = styled.div`
-  width: 35rem;
+const ShareButton = styled.button`
+  padding: 2px 0px 0px 0px;
+  background-color: #ffffff00;
+  font-size: 1.1rem;
+  border: none;
+  color: white;
 `;
-const HeaderLogo = styled.div`
+
+const HeaderSort = styled.div`
+  width: 41.7rem;
+`;
+const HeaderLogo = styled(Link)`
   color: white;
   font-size: 1.5rem;
   font-weight: 600;
+  text-decoration: none;
 `;
 
 export default HeaderItemComponent;
